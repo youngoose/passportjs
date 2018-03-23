430929961e1b7088e3489822690f65f7ddba87a6
youngoose <edwardjo811@gmail.com>
2018-03-23 18:06:05 +0000

    add secret.ejs

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require("express");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     res.render("home");
+});
+
+app.get("/secret", function(req, res){
+   res.render("secret"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
